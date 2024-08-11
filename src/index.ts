c1d4c68355b927ab28fe1a552d136c562b43b7ed
Checo <innovationhub.codingbydesign@gmail.com>
2024-08-11 12:19:35 -0600

    successful test of init'd backend server endpoint

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { Request, Response } from 'express'
+
+// send a test GET request to:
+//  -) read all the objects that are stored in my temp db
+
+const be_server = express();
+be_server.use(express.json());
+be_server.use(express.urlencoded({extended: true}));
+be_server.use(cors());
+
+be_server.get("/api/test", async (req: Request, res: Response) => {
+    res.json({message: "i hear your request"});
+});
+
+be_server.listen(7000, () => {
+    console.log("server running on localhost:7000");
+});
