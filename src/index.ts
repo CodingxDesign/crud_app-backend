6a26cb2d9a1f5e7ef8b6d961bfc1e80a0afdf865
Checo <innovationhub.codingbydesign@gmail.com>
2024-08-11 20:53:48 -0600

    successful HTTP Request tests, for resourcesAPI routeHandler

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { Request, Response } from 'express';
-import resourceRoutes from  './routes/resourceAPI'; 
-
-// send a test GET request to:
-//  -) read all the objects that are stored in my temp db
+import router from './routes/resourceAPI';
 
 const be_server = express();
 be_server.use(express.json());
 be_server.use(express.urlencoded({extended: true}));
 be_server.use(cors());
 
-be_server.use("/resources", resourceRoutes);
+be_server.use("/resources", router);
 
 be_server.listen(7000, () => {
     console.log("server running on localhost:7000");
--- src/routes/resourceAPI.ts
+++ src/routes/resourceAPI.ts
@@ -1,4 +1,5 @@
-import { Router } from 'express';
+import express from 'express';
+import cors from 'cors';
 import { randomUUID } from 'crypto';
 import { Request, Response } from 'express';
 
@@ -6,13 +7,15 @@ import { Request, Response } from 'express';
 const dbResources: any[] = [];
 
 // init server and add functionality
-const router = Router();
+const router = express.Router();
+router.use(express.json());
+router.use(cors());
 
 //  GET (all) - test that we get all the resources
 //     we should expect (if we get all resources):
 //         an array of objects 
 //         a 200 status code
-router.get('/resources', (req: Request, res: Response) => {
+router.get('/', (req: Request, res: Response) => {
     res.status(200).send(dbResources);
 });
 
@@ -23,7 +26,7 @@ router.get('/resources', (req: Request, res: Response) => {
 //           the server will send back the object
 //           the object returned should have the same uuid as the 
 //               param.id passed appended to the url
-router.get('/resources/:id', (req, res)=> {
+router.get('/:id', (req, res)=> {
     const id = req.params.id;
     const existing_dbResource = dbResources.find( 
         (ojbect) => ojbect.id === id
@@ -42,7 +45,7 @@ router.get('/resources/:id', (req, res)=> {
 //           a response with the object that was just created
 //           the object returned should have a uuid
 //           a 200 status code
-router.post('/resources', (req, res)=> {
+router.post('/', (req, res)=> {
     // guard against nothing to add sent in the request
     // if no info in body no need for runtime compiler to keep running
     if(!req.body) return res.status(400).send("no resource to add");
@@ -61,7 +64,7 @@ router.post('/resources', (req, res)=> {
 //       we should expect (if we update a resource):
 //           the server will send back a 200 status code
 //           the server will send back the object that was updated
-router.put('/resources/:id', (req, res)=> {
+router.put('/:id', (req, res)=> {
     // guard against missing body in request
     if(!req.body) return res.status(400).send("no resource to add");
     
@@ -100,7 +103,7 @@ router.put('/resources/:id', (req, res)=> {
 //           a response w/ the object that was deleted
 //           a status code: 404, if we attempt to GET the same object
 
-router.delete('/resources/:id', (req, res)=> {
+router.delete('/:id', (req, res)=> {
     const id = req.params.id;
     // define a funct to find an object by its id and return both the object and its index
     const getResourceandItsIndex = (): [Object | null, number] => {
