71af021f0cded36e113052341f4c2fa453f4f61b
Checo <innovationhub.codingbydesign@gmail.com>
2024-08-13 04:06:41 -0600

    handle requests to /universityCourse:id/register with universityRoutes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import router from './routes/resourceAPI';
+import universityCourseRouter from './routes/universityCourseRoutes'
 
 const be_server = express();
 be_server.use(express.json());
 be_server.use(express.urlencoded({extended: true}));
 be_server.use(cors());
 
-be_server.use("/resources", router);
+be_server.use("/courses", universityCourseRouter);
 
 app.listen(7000, () => {
     console.log("server running on localhost:7000");
